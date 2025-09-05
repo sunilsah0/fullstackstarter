68ea481c182949a42bdaedada1eea6d73b480eab
acshacsh <54509513+sunilsah0@users.noreply.github.com>
2025-09-05 18:03:30 +0200

    Update App.js

--- apps/web/src/App.js
+++ apps/web/src/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Fullstack Starter Web App</h1>
+      <p>Connected to API: {process.env.REACT_APP_API_URL}</p>
+    </div>
+  );
+}
+
+export default App;
 
